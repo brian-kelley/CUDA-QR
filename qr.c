7559d1d379daacfc4340ecee61325e1f0ac029ff
Brian Kelley <brian.honda11@gmail.com>
2018-04-22 17:13:10 -0500

    Finished writing initial serial implementation
    Not tested

--- qr.c
+++ qr.c
@@ -48,12 +48,18 @@ void mmqr(Scalar* mat, Scalar* tau, int m, int n)
       //The W matrix (for applying whole panel of HH reflectors at once)
       //should be in shared and is updated as each reflector is determined
       //
-      //the Y matrix is read from the subdiagonal part of the panel that was
-      //just computed (doesn't need separate explicit storage)
+      //TODO: columns of Y matrix are just the reflectors, so an explicit
+      //copy of it is unnecessary
       Scalar W[C][R];
+      Scalar Y[C][R];
       for(int i = 0; i < C; i++)
+      {
         for(int j = 0; j < R; j++)
+        {
           W[i][j] = 0;
+          Y[i][j] = 0;
+        }
+      }
       //update each trailing column (pr:pr+R, pc+C:N):
       //for each column, compute HH reflectors
       for(int col = 0; col < C; col++)
@@ -92,22 +98,49 @@ void mmqr(Scalar* mat, Scalar* tau, int m, int n)
           panel[col][i] /= u;
           v[i - vstart] = panel[col][i];
         }
-        //v is now fully computed
+        //v is now fully computed (explicitly)
         //update W matrix
-        //column 0 of W is special: can save most of the computation
-        if(col == 0)
+        //beta is from Algorithm 3 of Kerr
+        Scalar beta = 2 / innerProd;
+        //compute z = -Bv - WY^T * v
+        Scalar z[R];
+        for(int i = 0; i < R; i++)
         {
-          for(int i = wstart; i < wend; i++)
-          {
-            W[0][i] = (-2 / innerProd) * v[i - wstart];
-          }
+          z[i] = 0;
+          if(i >= vstart && i < vend)
+            z[i] = -beta * v[i - vstart];
         }
-        else
+        for(int i = 0; i < R; i++)
         {
-          for(int i = wstart; i < wend; i++)
+          //finish computing entry i of z
+          //compute zval as entry i of W * Y^T * v
+          Scalar zval = 0;
+          for(int j = 0; j < R; j++)
           {
-            W[col][i] = (-2 / innerProd) * v[i - wstart];
+            //need inner product of row i of W and column j of Y
+            //use the fact that only the first col+1 columns of W and Y are nonzero
+            if(j >= vstart && j < vend)
+            {
+              Scalar wyt = 0;
+              for(int k = 0; k < col + 1; k++)
+              {
+                wyt += W[k][i] * Y[j][k];
+              }
+              zval += wyt * v[j - vstart];
+            }
           }
+          z[i] -= beta * zval;
+        }
+        //z is the next column of W
+        for(int i = 0; i < R; i++)
+        {
+          W[col][i] = z[i];
+        }
+        //v is the next column of Y
+        //note that Y is zeroed out initially, so only need to copy nonzeros
+        for(int i = vstart; i < vend; i++)
+        {
+          Y[col][i] = v[i - vstart];
         }
         panelTau[col] = sign * u / norm;
         //apply reflector in col to remaining columns in panel
@@ -126,25 +159,49 @@ void mmqr(Scalar* mat, Scalar* tau, int m, int n)
         }
         free(v);
       }
-      //panel and panelTau are now both fully computed
+      //panel, panelTau, W and Y are all fully computed
       //write back panel to A
       for(int col = 0; col < C; col++)
       {
         memcpy(&mat[pr + pc * m], &panel[col][0], sizeof(Scalar) * R);
       }
-      //multiply each column by (I - tau * ww')
-      Scalar w[R];
+      //update trailing columns of A: A = (I + YW^T)A
+      //the new columns of A can be updated independently (and in parallel)
+      //so this loop could be a kernel launch with some A columns given to each SM
       for(int applyCol = pc + C; applyCol < n; applyCol++)
       {
-        for(int applyRow = pr; applyRow < pr + R; applyRow++)
+        //The new column, to be copied back into A
+        Scalar Acol[R];     //these vectors both go in shared
+        Scalar newAcol[R];
+        //gives perfect minimal memory bandwidth:
+        //each entry read/written once in optimally coalesced accesses
+        //the IA term above is implicit (other term added to this one)
+        for(int i = 0; i < R; i++)
         {
-          Scalar val = mat[applyCol][applyRow];
-          for(int i = 0; i < vlen; i++)
+          Acol[i] = mat[pr + i + applyCol * m];
+          newAcol[i] = Acol[i];
+        }
+        //now compute YW^T * A[<panel rows>, applyCol] and update newAcol
+        for(int i = 0; i < R; i++)
+        {
+          Scalar newAval = 0;
+          for(int j = 0; j < R; j++)
           {
-            Scalar wwVal;
-            val -= panelTau[applyCol] * v[windex] * v[i] * panel[applyCol][applyRow];
+            //need inner product of row i of Y and row j of W
+            Scalar ywt = 0;
+            for(int k = 0; k < C; k++)
+            {
+              ywt += Y[i][k] * W[j][k];
+            }
+            //multiply that by entry j of A
+            newAval += ywt * Acol[j];
           }
-          panel[applyCol][applyRow] = val;
+          newAcol[i] = += newAval;
+        }
+        //write back newAcol
+        for(int i = 0; i < R; i++)
+        {
+          mat[pr + i + applyCol * m] = newAcol[i];
         }
       }
     }
